0bb3b5d381a7fff486305eea6f7e3b4e7138b073
tmuspian <tmuspian@gmail.com>
2022-11-01 23:02:40 -0300

    first commit

---
+++ src/test/java/caldeira/stock/analyzer/StockAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package caldeira.stock.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
