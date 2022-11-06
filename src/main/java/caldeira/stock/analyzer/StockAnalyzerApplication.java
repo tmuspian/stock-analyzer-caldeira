b920ba7b233cb736605ba9ac3d8abf9458fc89c6
tmuspian <tmuspian@gmail.com>
2022-11-06 15:43:05 -0300

    Input stock

--- src/main/java/caldeira/stock/analyzer/StockAnalyzerApplication.java
+++ src/main/java/caldeira/stock/analyzer/StockAnalyzerApplication.java
@@ -2,13 +2,19 @@ package caldeira.stock.analyzer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 @SpringBootApplication
 public class StockAnalyzerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		System.out.println("Digite o codigo da ação que deseja pesquisar:");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String action = reader.readLine();
 		SpringApplication.run(StockAnalyzerApplication.class, args);
-		System.out.println("test 2");
+
 
 	}
 
